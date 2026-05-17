50c04eed4dffd2017bbfaefad333256ac89fab13
Abdu_Tello <abdulta@umich.edu>
2026-05-17 06:18:22 -0400

    make resident form the home page, add city worker login link to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="w-full border-b border-zinc-200 bg-white">
+    <header className="w-full border-b border-zinc-200 bg-white relative">
       <Link href="/" className="block">
         <Image
           src="/PotholePilotHeaderLogo.png"
@@ -13,6 +13,12 @@ export default function Header() {
           className="w-full h-auto"
           priority
         />
+      </Link>
+      <Link
+        href="/login"
+        className="absolute right-4 top-1/2 -translate-y-1/2 text-xs text-zinc-400 hover:text-zinc-600 transition-colors"
+      >
+        City Worker Login
       </Link>
     </header>
   );
